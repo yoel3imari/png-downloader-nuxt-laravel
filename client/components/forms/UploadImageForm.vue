5d7b22aa9354cb85198c2aa7221ce1f0bc0266dc
yoel3imari <youssef.elaimari@gmail.com>
2024-10-14 23:52:18 +0100

    upload form

---
+++ client/components/forms/UploadImageForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form action="">
+    <!-- title -->
+    <!-- description -->
+    <!-- category -->
+    <!-- tags -->
+    <!-- file -->
+  </form>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
